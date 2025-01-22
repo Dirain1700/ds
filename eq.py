739c106f5dc434cb36390eff4d9e64a70125ea08
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2025-01-22 17:46:24 +0900

    Updte eq.py

--- eq.py
+++ eq.py
@@ -4,13 +4,15 @@ c2 = int(input("c2を指定してください。"))
 
 if c1 == 0:
     print("警告: C1" + ("とC2は" if c2 == 0 else "は") + "0です。")
+    if c2 == 0:
+        print("警告: C1とC2は0です。")
+        exit()
+
+def getReadableSign(num):
+    return "" if num == 1 else "-" if abs(num) == 1 else num
 
 # 一次方程式 c1 * x + c0 = 0
-print(f"一次方程式 {str(c1)[:1] if abs(c1) == 1 else c1}x + {c0} = 0 の解は x = {-c0 / c1} です。")
-
-if c2 == 0:
-    print("警告: C2は0です。")
-    exit()
+print(f"一次方程式 {getReadableSign(c1)}x + {c0} = 0 の解は x = {-c0 / c1} です。")
 
 # 二次方程式 c2 * x ** 2 + c1 * x + c0 = 0
 # 判別式 D = b ** 2 - 4ac
@@ -20,12 +22,12 @@ if c2 == 0:
 D = c1 ** 2 - 4 * c2 * c0
 if D < 0:
     print("実数解なし")
-elif D > 0:
-    # 実数解が2つのとき。降順に解の公式で求める
-    x1 = (-c1 + D ** 0.5) / (2 * c2)
-    x2 = (-c1 - D ** 0.5) / (2 * c2)
-    print(f"二次方程式 {str(c2)[:1] if abs(c2) else c2}x ** 2 + {c1}x + {c0} = 0 の解は x = {x1}, {x2} です。")
-# else:
-    # 実数解が1つ, 重解
-    # x = -c1 / (2 * c2)
-    # print(f"二次方程式 {str(c2)[:1] if abs(c2) else c2}x ** 2 + {c1}x + {c0} = 0 の解は x = {x} です。")
+else:
+    # c2が0のとき、一次方程式
+    if c2 == 0:
+        print(f"一次方程式 {getReadableSign(c1)}x + {c0} = 0 の解は x = {-c0 / c1} です。")
+    else:
+        # 実数解が2つのとき。降順に解の公式で求める
+        x1 = (-c1 + D ** 0.5) / (2 * c2)
+        x2 = (-c1 - D ** 0.5) / (2 * c2)
+        print(f"二次方程式 {getReadableSign(c2)}x ** 2 + {getReadableSign(c1)}x + {c0} = 0 の解は x = {x1}, {x2} です。")
